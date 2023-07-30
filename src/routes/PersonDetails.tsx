992d29510131c31902e2cdbc75dde2f28774f94d
Catalin <g.ionutcatalin@protonmail.com>
2023-07-30 13:14:10 +0300

    feat: Delete contact

--- src/routes/PersonDetails.tsx
+++ src/routes/PersonDetails.tsx
@@ -1,15 +1,21 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useRef } from "react";
 import ArrowBack from "../assets/icons/ArrowBack";
-import { useAppSelector } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
 import Error from "./Error";
 import Envelope from "../assets/icons/Envelope";
 import Call from "../assets/icons/Call";
 import Webcam from "../assets/icons/Webcam";
 import Share from "../assets/icons/Share";
+import Trash from "../assets/icons/Trash";
+import { deleteContact } from "../features/contact/contactSlice";
 
 function PersonDetails() {
     const { id } = useParams();
+    const dialog = useRef<HTMLDialogElement>(null);
+    const navigate = useNavigate();
     const contacts = useAppSelector((state) => state.contact);
+    const dispatch = useAppDispatch();
     const currentContact = contacts.find(
         (contact) => contact.contactId === Number(id),
     );
@@ -26,8 +32,30 @@ function PersonDetails() {
         contactId,
     } = currentContact;
 
+    function showModal() {
+        dialog?.current?.showModal();
+    }
+
+    function handleClose() {
+        dialog?.current?.close();
+    }
+
+    function handleDeleteContact() {
+        dispatch(
+            deleteContact({
+                contactId,
+                contactName,
+                contactGroup,
+                contactPhoneNumber,
+                contactFile,
+            }),
+        );
+        dialog?.current?.close();
+        navigate("/");
+    }
+
     return (
-        <div className="flex h-full flex-col rounded-3xl bg-gradient-to-tr from-purple-400 via-blue-300 to-indigo-500 md:max-h-[50vh]">
+        <div className="flex h-full flex-col rounded-3xl bg-gradient-to-tr from-purple-400 via-blue-300 to-indigo-500 md:max-h-[60vh]">
             <div className="flex min-h-[50%] flex-col items-start rounded-t-3xl px-6 py-8 text-white md:pt-12">
                 <div className="flex w-full">
                     <Link to="/" className="flex w-full items-center font-bold">
@@ -118,7 +146,43 @@ function PersonDetails() {
                 <div className="border-b border-slate-200 py-2">
                     <p className="text-sm font-black">Share my location</p>
                 </div>
+                <button
+                    type="button"
+                    className="flex items-center justify-between border-b border-slate-200 py-2"
+                    onClick={showModal}
+                >
+                    <p className="text-sm font-black text-red-500">
+                        Delete contact
+                    </p>
+                    <Trash width={18} height={18} className="text-red-500" />
+                </button>
             </div>
+
+            <dialog ref={dialog} className="delete-modal rounded-2xl">
+                <div className="flex flex-col items-center justify-center p-10">
+                    <p className="pb-4 text-3xl font-bold">Delete contact</p>
+                    <p className="text-xl text-slate-600">
+                        Are you sure you want to delete contact &quot;
+                        {currentContact.contactName}&quot;?
+                    </p>
+                    <div className="flex w-full flex-col justify-between pt-5">
+                        <button
+                            onClick={handleClose}
+                            type="button"
+                            className="mt-4 flex items-center justify-center rounded-3xl border border-slate-200 px-4 py-2 transition-all hover:bg-slate-200"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            onClick={handleDeleteContact}
+                            type="button"
+                            className="mt-4 flex items-center justify-center rounded-3xl bg-red-500 px-4 py-2 text-white transition-all hover:bg-red-700"
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 }
