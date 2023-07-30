c1c482bacce246fbfef62ae60ff0513292af1b0f
Catalin <g.ionutcatalin@protonmail.com>
2023-07-30 12:02:57 +0300

    feat: Edit contact & refactor

--- src/features/contact/contactSlice.ts
+++ src/features/contact/contactSlice.ts
@@ -1,12 +1,12 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"; // Define a type for the slice state
 
 // Define a type for the slice state
 interface ContactState {
     contactId: string | number;
-    contactFile?: string;
     contactGroup: string;
     contactName: string;
     contactPhoneNumber: string | number;
+    contactFile?: string;
 }
 
 // Define the initial state using that type
@@ -20,17 +20,21 @@ export const contactSlice = createSlice({
             return [...state, action.payload];
         },
         updateContact: (state, action: PayloadAction<ContactState>) => {
-            return [...state, action.payload];
-        },
+            const contact = state.find(
+                ({ contactId }) => contactId === action.payload.contactId,
+            );
 
-        // Use the PayloadAction type to declare the contents of `action.payload`
-        // incrementByAmount: (state, action: PayloadAction<number>) => {
-        //     state.value += action.payload;
-        // },
+            if (contact) {
+                contact.contactName = action.payload.contactName;
+                contact.contactPhoneNumber = action.payload.contactPhoneNumber;
+                contact.contactGroup = action.payload.contactGroup;
+                contact.contactFile = action.payload.contactFile;
+            }
+        },
     },
 });
 
-export const { createContact } = contactSlice.actions;
+export const { createContact, updateContact } = contactSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 // export const selectContact = (state: RootState) => state.contact.name;
